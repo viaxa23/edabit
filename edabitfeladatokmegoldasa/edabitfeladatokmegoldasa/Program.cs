b04ee9ba1b2ada0861f72ccd33eca04387859f88
visnyei.adam <visnyei.adam@O16G13>
2020-01-27 08:40:37 +0100

    csonkolt

---
+++ edabitfeladatokmegoldasa/edabitfeladatokmegoldasa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edabitfeladatokmegoldasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem a stringet: ");
+            string bemeno = Console.ReadLine();
+            Console.WriteLine($"A csonkolt sztring: {Remove_1(bemeno)}");
+            Console.ReadKey(true);   
+        }
+        static string Remove_1(string bemeno)
+        {
+            if (bemeno.Length <= 2)
+            {
+                return bemeno;
+            }
+            else
+            {
+                return bemeno.Remove(0, 1).Remove(bemeno.Length -2, 1);
+            }
+        }
+            static string Remove_2(string bemeno)
+        {
+                if (bemeno.Length <= 2)
+                {
+                    return bemeno;
+                }
+                else
+                {
+                    return bemeno.Substring(1, bemeno.Length-2);
+                }
+
+            }
+    }
+}
