95032973fc704e963c408fe3c25ef90d5e2bcf7c
visnyei.adam <visnyei.adam@O16G13>
2020-01-27 11:10:45 +0100

    harmadik

---
+++ phoneformat/phoneformat/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoneformat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Be: ");
+            string be = Console.ReadLine();
+            Console.WriteLine(format(be));
+            Console.ReadKey(true);
+        }
+
+        static string format(string be)
+        {
+            return "(" + be.Substring(0, 3) + ")" + be.Substring(3, 3) + "-" + be.Substring(6, 4);
+        }
+    }
+}
---
+++ phoneformat/phoneformat/telefonszam.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phoneformat
+{
+    class telefonszam
+    {
+        public int[] Telefon { get; set; }
+
+        public telefonszam(int[] telefon)
+        {
+            this.Telefon = telefon;
+        }
+
+        public string Telefonszam()
+        {
+            string kimeno = "";
+
+            for (int i = 0; i < this.Telefon.Length; i++)
+            {
+                kimeno = String.Concat(kimeno, this.Telefon[i]);
+
+            }
+
+            kimeno = String.Concat("(", kimeno.Substring(0, 3), ")", kimeno.Substring(3, 3), "-", kimeno.Substring(6));
+            return kimeno;
+        }
+    }
+}
